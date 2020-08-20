277fb890b728075f6225ea942a1aa95969ec1218
Karishma1208 <60914943+Karishma1208@users.noreply.github.com>
2020-08-20 14:54:03 +0530

    payment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css'; 
+import Payment from './components/Payment';
+
+function App() {
+  return (
+  
+    <div>
+    <Payment/>
+    </div>
+    
+  );
+}
+
+export default App;
